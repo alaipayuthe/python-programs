8063635d499c70b8cfeb6074c4b76cecff68e29a
“Honey <lullaby.techie@gmail.com>
2018-11-15 16:28:32 +0530

    Name and age tagging program

---
+++ name_age_tag.py
@@ -0,0 +1,6 @@
+"""
+Write a Python expression that creates the string "Joe Warren is 56 years old." from several strings including "Joe Warren" and the number 56 and then prints the result (Hint: Use the function str to convert the number into a string.)
+"""
+
+# Name and age
+print("Joe Warren is " + str(56) + " years old")
