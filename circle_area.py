7cc47309937f85ed415ebf0a0b766bb5a46d9e34
“Honey <lullaby.techie@gmail.com>
2018-11-15 15:58:34 +0530

    Circle area calculator program

---
+++ circle_area.py
@@ -0,0 +1,6 @@
+"""
+The area of a circle is πr2 where r is the radius of the circle. (The raised 2 in the formula is an exponent.) Write a Python statement that calculates and prints the area in square inches of a circle whose radius is 8 inches. Assume that the constant π=3.14
+"""
+
+# Calculate area of circle
+print("Area of circle = " + str((3.14 * 8 * 8)) + " square inches")
