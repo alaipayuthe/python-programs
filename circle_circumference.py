11184a8f9e840bfa9b4287d6daf168d1ee251acb
“Honey <lullaby.techie@gmail.com>
2018-11-15 15:52:50 +0530

    Circle circumference calculator program

---
+++ circle_circumference.py
@@ -0,0 +1,8 @@
+"""
+The circumference of a circle is 2πr where r is the radius of the circle. Write a Python statement that calculates and prints the circumference in inches of a circle whose radius is 8 inches. Assume that the constant π=3.14
+"""
+
+# Calculate circumference of a circle
+print("Circumference of circle = " + str((2 * 3.14 * 8)) + " inches")
+
+
