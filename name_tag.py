10a75cfc204dfdc5af55c5347281c3fba052d1f0
“Honey <lullaby.techie@gmail.com>
2018-11-15 16:14:19 +0530

    Name tagging program

---
+++ name_tag.py
@@ -0,0 +1,7 @@
+"""
+Write a single Python statement that combines the three strings "My name is", "Joe" and "Warren" (plus a couple of other small strings) into one larger string "My name is Joe Warren." and prints the result. (Hint: Experiment with adding two strings in Python using the + operator.) 
+"""
+
+# Tag names
+print("My name is " + " Joe" + " Warren")
+
