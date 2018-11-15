25bd1022111b67df48365e54c92a82025a3b2c39
“Honey <lullaby.techie@gmail.com>
2018-11-15 12:30:02 +0530

    Hours to seconds converter program

---
+++ hours_to_secs.py
@@ -0,0 +1,7 @@
+"""
+Write a Python statement that calculates and prints the number of seconds in 7 hours, 21 minutes and 37 seconds
+"""
+
+# Hours to seconds converter
+print("7 hours 21 minutes 37 seconds converted to seconds: " + str((7*60*60) + (21*60) + 37))
+
