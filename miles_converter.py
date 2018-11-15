63310c4f16c18fb1b6077c0e823757719048dd93
“Honey <lullaby.techie@gmail.com>
2018-11-15 12:14:07 +0530

    My first Python program

---
+++ miles_converter.py
@@ -0,0 +1,13 @@
+"""
+There are 5280 feet in a mile. Write a Python statement that calculates and prints the number of feet in 13 miles.
+"""
+
+# Miles to feet converter program
+miles_in_feet = 5280
+
+# compute feet for 13 miles
+convert_13_miles_to_feet = 13 * 5280
+
+print("13 miles = " + str(convert_13_miles_to_feet))
+
+
