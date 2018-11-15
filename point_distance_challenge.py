7f3c5aa95a88e38e9a3b1b6419c026516fbe1c17
“Honey <lullaby.techie@gmail.com>
2018-11-15 16:35:31 +0530

    Point distance challenge

---
+++ point_distance_challenge.py
@@ -0,0 +1,7 @@
+"""
+Challenge: The distance between two points (x0,y0) and (x1,y1) is sqrt((x0−x1)^2+(y0−y1)^2). Write a Python statement that calculates and prints the distance between the points (2,2) and (5,6). (Hint: Remember that a square root can be computed by raising a value to the 0.5 power.)
+"""
+
+#Point distance challenge
+print("Distance between points: " + str((((((2 - 5) ** 2) + ((2 - 6) ** 2))) ** 0.5)))
+
