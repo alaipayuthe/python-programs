31d8c20727b15b2592cfd6f1b5ca9e6efd89f73c
“Honey <lullaby.techie@gmail.com>
2018-12-19 11:43:44 +0530

    Data Analysis Week1 Exercises

---
+++ Python_Data_Analysis/Week1_Dictionaries/Exercises/pgm1_empty_dict.py
@@ -0,0 +1,13 @@
+"""
+Initialize a dictionary my_dict to be empty
+"""
+# Create an empty dictionary
+my_dict = dict()
+
+# Tests
+print(type(my_dict))
+print(my_dict)
+
+# Output
+#<class 'dict'>
+#{}
---
+++ Python_Data_Analysis/Week1_Dictionaries/Exercises/pgm6_dict_value_sum.py
@@ -0,0 +1,24 @@
+"""
+Write a function value_sum(my_dict) that
+returns the sum of the values in a dictionary
+"""
+
+def value_sum(my_dict):
+    """
+    Given a dictionary my_dict whose values are numbers, return
+    the sums of the values in my_dict
+    """
+    dict_sum = 0
+    for value in my_dict.values():
+        dict_sum = dict_sum + value
+    return dict_sum
+
+# Testing code
+print(value_sum({}))
+print(value_sum({0 : 1}))
+print(value_sum({"Joe" : 1, "Scott" : 2, "John" : 4}))
+
+# Output
+#0
+#1
+#7
