e03942aaef04138623e546bfd40ea92694ce623e
“Honey <lullaby.techie@gmail.com>
2018-12-29 17:29:19 +0530

    Data Analysis Week3 Assignment - Intermediate

---
+++ Python_Data_Analysis/Week3_TabularData_CSVFiles/Assignment/isp_read_write_csvfiles.py
@@ -0,0 +1,77 @@
+"""
+Project for Week 3 of "Python Data Analysis".
+Read and write CSV files using a dictionary of dictionaries.
+
+Be sure to read the project description page for further information
+about the expected behavior of the program.
+"""
+
+import csv
+
+def read_csv_fieldnames(filename, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Ouput:
+      A list of strings corresponding to the field names in
+      the given CSV file.
+    """
+    fieldnames_list = list()
+    # Open and read the csv file, return the field names in the file
+    with open(filename, "rt") as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=separator, quotechar=quote, quoting=csv.QUOTE_NONE)
+        fieldnames_list.extend(csv_reader.fieldnames)
+
+    return fieldnames_list
+
+print(read_csv_fieldnames("table1.csv", ',', None))
+print(read_csv_fieldnames("table2.csv", ',', "\""))
+print(read_csv_fieldnames("table3.csv", ',', "'"))
+
+
+def read_csv_as_list_dict(filename, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Output:
+      Returns a list of dictionaries where each item in the list
+      corresponds to a row in the CSV file.  The dictionaries in the
+      list map the field names to the field values for that row.
+    """
+    return []
+
+
+def read_csv_as_nested_dict(filename, keyfield, separator, quote):
+    """
+    Inputs:
+      filename  - name of CSV file
+      keyfield  - field to use as key for rows
+      separator - character that separates fields
+      quote     - character used to optionally quote fields
+    Output:
+      Returns a dictionary of dictionaries where the outer dictionary
+      maps the value in the key_field to the corresponding row in the
+      CSV file.  The inner dictionaries map the field names to the
+      field values for that row.
+    """
+    return {}
+
+
+def write_csv_from_list_dict(filename, table, fieldnames, separator, quote):
+    """
+    Inputs:
+      filename   - name of CSV file
+      table      - list of dictionaries containing the table to write
+      fieldnames - list of strings corresponding to the field names in order
+      separator  - character that separates fields
+      quote      - character used to optionally quote fields
+    Output:
+      Writes the table to a CSV file with the name filename, using the
+      given fieldnames.  The CSV file should use the given separator and
+      quote characters.  All non-numeric fields will be quoted.
+    """
+    pass
